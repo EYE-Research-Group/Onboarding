a6ab5b68dfecd13a68ff66378e92f70758319b50
Uyen Hoang <minhuyen0512@gmail.com>
2022-04-29 13:34:20 -0700

    Create the onboarding page.

---
+++ docs/onboarding.md
@@ -0,0 +1,52 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Onboarding
+
+Welcome all new members to **Educating Young Eyes Research Group**.
+
+This document provides you information to get you on board with the group and start contributing to our amazing projects!
+
+## Week 1: Getting Started
+
+### What you'll need to do
+
+- Request access to our **Google Drive** by contacting Professor Erdly via email.
+- Get invited as a member to our **GitHub organization** and general **Trello board** by contacting any current member.
+- Click the **Discord** link below in the footer to join our sercer!
+- **Save the Zoom meeting link** that you have been sent via email.
+
+**Note: We recommend that you use your personal email address for this because if you still want to stick around even after you have graduated, your UW email will be deleted and you will lose any access.**
+
+### Your first meeting
+
+On your first meeting, each team member will introduce themselves and talk briefly about their teams as well as their current projects.
+
+Let everyone know about your skillsets and which area you would like to get involved. The team will reach out to you if they want you, or you can decide which team you want to join. It's up to you!
+
+## Week 2: The Teams
+
+After you have been added to our GitHub, you can take a look at all repositories to see which project sparks your interest.
+
+Currently, we have **8 teams** with different projects and focuses. Each team has their own **Discord channel** so ping them if you want more information!
+
+- AWS Backend: Focus on developing services for every application.
+- Game: Creating fun and interactive games for the eyes.
+- QuickCheck: Developing a mobile application to perform vision screening.
+- Toolbox: Focus on a set of tools defined by Dr. Pearson.
+- VR: A virtual reality application for vision.
+- UI/UX: Design and prototype websites and applications.
+
+### What's next?
+
+Once you have on your mind which team you would like to join and which project you would like to work on, **set up a meeting** with the team to get more in-depth information.
+
+## Week 3: Settle In
+
+After your meeting with the team, make sure to read through any documentation of the team to set up your local environment.
+
+Start exploring the project, playing around with it (and read through the code) until you get the hang of it.
+
+Then you can work on your first task and contribute to our projects!
---
+++ docusaurus.config.js
@@ -0,0 +1,103 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+	title: 'Educating Young Eyes Research Group',
+	tagline: 'Dinosaurs are cool',
+	url: 'https://github.com',
+	baseUrl: '/onboarding/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'EYE-Research-Group', // Usually your GitHub org/user name.
+	projectName: 'Onboarding', // Usually your repo name.
+
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					routeBasePath: '/', // Serve the docs at the site's root
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: 'EYE Research Group',
+				logo: {
+					alt: 'My Site Logo',
+					src: 'img/logo.svg',
+				},
+				items: [
+					{
+						type: 'doc',
+						docId: 'onboarding',
+						position: 'left',
+						label: 'Tutorial',
+					},
+					{ to: '/blog', label: 'Blog', position: 'left' },
+					{
+						href: 'https://github.com/facebook/docusaurus',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Discord',
+								href: 'https://discordapp.com/invite/docusaurus',
+							},
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'Blog',
+								to: '/blog',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/facebook/docusaurus',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Educating You Eyes Research Group. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
+};
+
+module.exports = config;
